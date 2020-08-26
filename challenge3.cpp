1c14a525f3567543fb54fe4942497d637ce8a898
Harshad Dhane <59340082+Harshad141@users.noreply.github.com>
2020-08-26 14:23:17 +0530

    Create challenge3.cpp

---
+++ challenge3.cpp
@@ -0,0 +1,16 @@
+code:
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int x;
+    long y;
+    char z;
+    float a;
+    double b;
+    scanf("%d %ld %c %f %lf",&x,&y,&z,&a,&b);
+    printf("%d\n%ld\n%c\n%f\n%lf",x,y,z,a,b);
+}
