fd3bc817c4351e386746fa5dfdc837f6b9114656
Harshad Dhane <59340082+Harshad141@users.noreply.github.com>
2020-09-24 21:02:20 +0530

    Create array introduction.cpp

---
+++ array introduction.cpp
@@ -0,0 +1,35 @@
+revrse an array
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+  
+    int arr[50], size, i, j, temp;
+    cin>>size;
+    for(i=0; i<size; i++)
+    {
+        cin>>arr[i];
+    }
+    j=i-1;  // now j will point to the last element
+    i=0;   //  and i will be point to the first element
+    while(i<j)
+    {
+        temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+        i++;
+        j--;
+    }
+    for(i=0; i<size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+    return 0;
+}
