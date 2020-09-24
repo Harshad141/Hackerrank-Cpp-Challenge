353281563c45f10ade7ec52f9ffc04430909de2c
Harshad Dhane <59340082+Harshad141@users.noreply.github.com>
2020-09-24 20:45:33 +0530

    Update challenge6.cpp

--- challenge6.cpp
+++ challenge6.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 #include <cstdio>
+#include<math.h>
 using namespace std;
 
 /*
 Add `int max_of_four(int a, int b, int c, int d)` here.
 */
+int max_of_four(int a, int b, int c, int d){
+    int max=a;
+    int arr[4]={a,b,c,d};
+    for(int i;i<=4;i++)
+    {
+      if(max <arr[i])
+      {
+        max=arr[i];
+      }
+    }
+    return max;
+    
+}
+
+
 
 int main() {
-    int a, b, c, d;
-    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
     int ans = max_of_four(a, b, c, d);
-    printf("%d", ans);
+    cout<<ans;
     
     return 0;
 }
